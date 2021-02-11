e1c3f9004e10d6491dc1ba8aba3a03ab9edfb497
naok1207 <zenos.game.anime.all.vo1@gmail.com>
2021-02-11 15:15:08 +0900

    :sparkles: sorcery導入 + Userモデル作成

---
+++ db/migrate/20210210125428_sorcery_core.rb
@@ -0,0 +1,15 @@
+class SorceryCore < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :username,         null: false
+      t.string :email,            null: false
+      t.string :crypted_password
+      t.string :salt
+
+      t.timestamps                null: false
+    end
+
+    add_index :users, :email, unique: true
+    add_index :users, :username, unique: true
+  end
+end
